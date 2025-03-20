2f15a368b069137bc273e64ce39956f4223bf57f
luansilvafatec <luan.silva99@fatec.sp.gov.br>
2025-03-20 11:03:03 -0300

    estudo sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function () {
+    return "O código foi testado";
+});
+
+Route::get('/usuario/{nome}', function ($nome) {
+    return "O usuário atual é: ".$nome;
+});
+
+Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
+    return "A soma é: ". $num1 + $num2;
+});
+
