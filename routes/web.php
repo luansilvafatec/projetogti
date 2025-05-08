8a96c6e607ddff58031b783f914ecd384e677a01
luansilvafatec <luan.silva99@fatec.sp.gov.br>
2025-05-08 09:26:45 -0300

    tela de inicio e atividade

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,7 +11,8 @@ Route::get('/', function () {
 
 Route::get('/criar-conta', function () {
     return view('criar-conta');
-});
+})->name('criar-conta');
+
 Route::post('/salva-conta',
 function (Request $request) {
     //dd($request);
@@ -20,20 +22,32 @@ function (Request $request) {
     $user->password = $request->password;
     $user->save();
 
-    return "Usuário salvo com sucesso";
+    return redirect(route('login'));
 
 
 })->name('salva-conta');
 
-Route::get('/teste', function () {
-    return "O código foi testado";
-});
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
 
-Route::get('/usuario/{nome}', function ($nome) {
-    return "O usuário atual é: ".$nome;
-});
+Route::post('/logar', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
 
-Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
-    return "A soma é: ". $num1 + $num2;
-});
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
 
+        return redirect()->intended('dashboard');
+    }
+
+    return back()->withErrors([
+        'email' => 'O email e senha digitados não são válidos',
+    ])->onlyInput('email');
+})->name('logar');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
